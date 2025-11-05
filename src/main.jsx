b43d2827b30e88d6eebe1e37eddd18e57abf313b
PerezLucas02 <perezlucasf2@gmail.com>
2025-11-05 18:32:13 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { TaskProvider } from "./context/TaskContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <TaskProvider>
+          <App />
+        </TaskProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
